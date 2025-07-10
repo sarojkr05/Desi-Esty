0d9a7cbf12cbf796838fda61b16e08e7f65cdede
Saroj Kumar Das <sarojsarojkumar753@gmail.com>
2025-07-10 19:49:08 +0530

    Made changes in backend and created dashboard for artisan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import NotFoundPage from "./pages/NotFoundPage"
 import DeniedPage from "./pages/DeniedPage"
+import ArtisanDashboard from "./pages/ArtisanDashboard"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard/artisan" element={<ArtisanDashboard />} />
 
         <Route path="/denied" element={<DeniedPage />} />
         <Route path="*" element={<NotFoundPage />} />
