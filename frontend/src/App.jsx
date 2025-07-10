51d98865f3dcc3584dd3fe17d48a753fd4682a8d
nish-a-pawar <nisha.pawar2624@gmail.com>
2025-07-10 21:50:11 +0530

    UI - Login and Register Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,18 @@ import HomePage from "./pages/HomePage"
 import NotFoundPage from "./pages/NotFoundPage"
 import DeniedPage from "./pages/DeniedPage"
 import ArtisanDashboard from "./pages/ArtisanDashboard"
-
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
 function App() {
 
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+         <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard/artisan" element={<ArtisanDashboard />} />
-
+     
         <Route path="/denied" element={<DeniedPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
