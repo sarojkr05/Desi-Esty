fbea656e7133fb943fe890afb7a66006f4dc1cd6
Aftab Ali <aftab.ali.96088@gmail.com>
2025-07-15 12:06:18 +0530

    crud operation fixed

--- backend/src/services/productService.js
+++ backend/src/services/productService.js
@@ -17,26 +17,17 @@ export const getArtisanProducts = async (artisanId) => {
 
 export const editProduct = async (id, artisanId, updatedData) => {
     const product = await findProductById(id);
-
-
-
-
     if (!product || product.artisan.toString() !== artisanId.toString()) {
         throw new Error("Not authorized or product not found");
     }
-
     return await updateProduct(id, updatedData);
 };
 
 
-
 export const removeProduct = async (id, artisanId) => {
     const product = await findProductById(id);
-
-
     if (!product || product.artisan.toString() !== artisanId.toString()) {
         throw new Error("Not authorized or product not found");
     }
-
     return await deleteProduct(id);
 };
